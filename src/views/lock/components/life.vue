bcac5741bde65cadf7515337149c30791ac6c497
unknown <liushuhao@sendcloud.im>
2019-03-26 15:40:39 +0800

    life

--- src/views/lock/components/life.vue
+++ src/views/lock/components/life.vue
@@ -8,10 +8,17 @@
       <span class="active">XXX</span>
     </div>
     <div class="content"> 
-      <div v-for="(item, index) in list" :key="index">
-        <el-select>
-          
+      <div v-for="(item, index) in list" :key="index" class="item">
+        <el-select v-model="value" placeholder="请选择" class="select" @change="onSelect">
+          <el-option v-for="each in item.options" :key="each.value" :label="each.label" :value="each.value"></el-option>
         </el-select>
+        <div class="life-no-data">
+          <img src="../../../assets/cirque.png" alt="">
+          <p>{{value}}</p>
+          <p>{{item.options.label}}</p>
+          <p>{{item.num}}</p>
+          <img class="connect" src="../../../assets/connect.png" alt="">
+        </div>
       </div>
     </div>
   </div>
@@ -21,7 +28,43 @@
 export default {
   name: '',
   data() {
-    return {}
+    return {
+      value: 0,
+      list: [{
+        name: '注册',
+        num: 100,
+        options: [{label: '注册', value: 0}, {label: '未注册', value: 1}],
+      }, {
+        num: 100,
+        options: [{label: '安装', value: 0}, {label: '未安装', value: 1}],
+      }, {
+        num: 100,
+        options: [{label: '验收', value: 0}, {label: '未验收', value: 1}],
+      }, {
+        num: 100,
+        options: [{label: '在线', value: 0}, {label: '离线', value: 1}],
+      }, {
+        num: 100,
+        options: [{label: '在线', value: 0}, {label: '离线', value: 1}],
+      }, {
+        num: 100, 
+        options: [{label: '数据正常', value: 0}, {label: '数据异常', value: 1}],
+      }, {
+        num: 100,
+        options: [{label: '数据（无维修）', value: 0}],
+      }, {
+        num: 100,
+        options: [{label: '数据（无注销）', value: 0}],
+      }, {
+        num: 100,
+        options: [{label: '数据（无报废）', value: 0}],
+      }]
+    }
+  },
+  methods: {
+    onSelect(val) {
+      console.log('val', val)
+    }
   },
   components: {}
 }
@@ -30,6 +73,9 @@ export default {
 <style scoped lang="stylus">
   @import '~styles/mixin'
   @import '~styles/variable'
+  .el-input--suffix .el-input__inner
+    padding 12px!important
+
   .life
     position relative
     z-index 1
@@ -45,6 +91,38 @@ export default {
       .active 
         margin 0 20px 0 10px
         color $color-active
-
+    .content
+      display flex
+      .item
+        margin-right 45px
+        text-align center
+        .select
+          width 120px
+          position relative
+          left 13px
+        .life-no-data
+          position relative
+          img:nth-of-type(1) 
+            width 120%
+          p:nth-of-type(1)
+            position absolute
+            top 47px
+            left 53px
+            font-size $font-small
+          p:nth-of-type(2)
+            position absolute
+            top 73px
+            left 53px
+            font-size $font-small
+            color $color-active
+          .connect
+            position absolute
+            width 45%
+            top 55px
+            left 144px
+      .item:last-of-type
+        margin-right 0
+        .connect
+          display none
 
 </style>
